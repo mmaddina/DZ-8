a3a9786e4360edb56b0f29fd1bde6b624cd5b002
mmaddina <mamedovamadinaa8@gmail.com>
2023-01-03 17:58:56 +0300

    Task 58 done

--- Program.cs
+++ Program.cs
@@ -67,7 +67,7 @@ void PrintArray(int[,] array)
 //8 4 2 4
 //5 2 6 7
 //Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
-
+/*
 void RandomArray(int[,] array)
 {
     for (int i = 0; i < array.GetLength(0); i++)
@@ -117,7 +117,7 @@ RandomArray(table);
 PrintArray(table);
 Console.WriteLine();
 MinSumRowNum(table);
-
+*/
 
 
 
@@ -128,8 +128,71 @@ MinSumRowNum(table);
 //Результирующая матрица будет:
 //18 20
 //15 18
+/*
+int rows = ReadInt("Введите количество строк: ");
+int columns = ReadInt("Введите количество столбцов: ");
+int[,] array = new int[rows, columns];
+int[,] secondArray = new int[rows, columns];
+int[,] resultArray = new int[rows, columns];
 
+ArrayRandom(array);
+PrintArray2D(array);
 
+Console.WriteLine();
+
+ArrayRandom(secondArray);
+PrintArray2D(secondArray);
+
+Console.WriteLine();
+
+if (array.GetLength(0) != secondArray.GetLength(1))
+{
+    Console.WriteLine(" Нельзя перемножить ");
+    return;
+}
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < secondArray.GetLength(1); j++)
+    {
+        resultArray[i, j] = 0;
+        for (int k = 0; k < array.GetLength(1); k++)
+        {
+            resultArray[i, j] += array[i, k] * secondArray[k, j];
+        }
+    }
+}
+
+PrintArray2D(resultArray);
+
+int ReadInt(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void ArrayRandom(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+*/
 
 //Задача 60.Сформируйте трёхмерный массив из неповторяющихся двузначных чисел. Напишите программу, которая будет построчно выводить массив, добавляя индексы каждого элемента.
 //Массив размером 2 x 2 x 2
